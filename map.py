5dffad0630abe4e8a82259e0e24367ccf642d873
Rory Stokes <helix@forscience.com.au>
2013-04-27 14:45:51 +0930

    polygons added

--- map.py
+++ map.py
@@ -1,11 +1,10 @@
 import geometry
 import mapping
 from node import Node
+import unittest
 
-print "start"
 top_left_inner = geometry.Polygon((4, 10), (10, 4), (10, 10),
                                ccw=False)
-print "1"
 top_right_inner = geometry.Polygon((22,4), (28,10), (22,10),
 								ccw=False)
 bot_left_inner = geometry.Polygon((4,22), (10,28), (4,28),
@@ -13,7 +12,7 @@ bot_left_inner = geometry.Polygon((4,22), (10,28), (4,28),
 bot_right_inner = geometry.Polygon((22,22), (28,22), (22,28),
 								ccw=False)
 
-bot_left_outer = geometry.Polygon((0,25),(0,32),(32,32),
+bot_left_outer = geometry.Polygon((0,25),(32,32),(0,32),
 								ccw=False)
 bot_right_outer = geometry.Polygon((32,25),(32,32),(25,32),
 								ccw=False)
@@ -23,8 +22,6 @@ river_top = geometry.Polygon((0,13),(32,13),(32,14),(0,14),
 river_bot = geometry.Polygon((0,18),(32,18),(32,19),(0,19),
 								ccw=False)
 
-
-print "polys made"
 terrain_map = mapping.Board()
 
 terrain_map.add(top_left_inner)
@@ -33,4 +30,3 @@ terrain_map.add(bot_left_inner)
 terrain_map.add(bot_right_inner)
 terrain_map.add(bot_left_outer)
 terrain_map.add(bot_right_outer)
-print "map done"
