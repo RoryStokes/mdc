fafb3cd65babc5169c545bced7d719563144c1f3
Myles Clark <blokart11@gmail.com>
2013-04-27 17:10:50 +0930

    Implemented basic input for the mouse.

--- main.py
+++ main.py
@@ -2,24 +2,25 @@ import geometry
 import mapping
 from node import Node
 import unittest
-import render, event
-import pygame
+import render, event, inputs
+import pygame, sys
 from unit import Unit
+from pygame.locals import *
 
 map_obstructions = [
-[(4, 10), (10, 4), (10, 10)],		#top left inner
-[(22,4), (28,10), (22,10)],			#top right inner
-[(4,22), (10,22), (10,28)],			#bottom left inner
-[(22,22), (28,22), (22,28)],		#bottom right inner
-[(0,26),(6,32),(0,32)],				#bottom left outer
-[(26,0),(32,0),(32,6)],				#top right outer
-[(4,13),(28,13),(28,14),(4,14)],	#river top boundary
-[(4,18),(28,18),(28,19),(4,19)]]	#river bot boundary
+[(4, 10), (10, 4), (10, 10)],       #top left inner
+[(22,4), (28,10), (22,10)],         #top right inner
+[(4,22), (10,22), (10,28)],         #bottom left inner
+[(22,22), (28,22), (22,28)],        #bottom right inner
+[(0,26),(6,32),(0,32)],             #bottom left outer
+[(26,0),(32,0),(32,6)],             #top right outer
+[(4,13),(28,13),(28,14),(4,14)],    #river top boundary
+[(4,18),(28,18),(28,19),(4,19)]]    #river bot boundary
 
 map_board = mapping.Board()
 
 for poly in map_obstructions:
-	map_board.add( geometry.Polygon(*poly, ccw=False) )
+    map_board.add( geometry.Polygon(*poly, ccw=False) )
 
 pygame.init()
 clock   = pygame.time.Clock()
@@ -29,9 +30,26 @@ pygame.display.set_caption("MDC")
 units = [Unit(5, 5)]
 renderer = render.Renderer(window, map_obstructions, units)
 event_manager.register("update", renderer.update)
+inputManager = inputs.InputManager(event, renderer)
 
-while True:	
-	event_manager.notify("update")
-	event_manager.update()
-	pygame.display.update()
-	clock.tick(30)
+while True: 
+    event_manager.notify("update")
+    event_manager.update()
+    pygame.display.update()
+
+    for e in pygame.event.get():
+        if e.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif e.type == VIDEORESIZE:
+            pygame.display.set_mode((e.size),pygame.RESIZABLE)
+        elif e.type == MOUSEBUTTONDOWN:
+            inputManager.registerClick(e.pos, e.button, True)
+        elif e.type == MOUSEBUTTONUP:
+            inputManager.registerClick(e.pos, e.button, False)
+        elif e.type == KEYDOWN:
+            inputManager.registerKey(e.key, True)
+        elif e.type == KEYUP:
+            inputManager.registerKey(e.key, False)
+
+    clock.tick(30)
